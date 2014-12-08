0903a420fe5d42a51d29c03c41937b448ad51109
Spaceenter <swyddz1989@gmail.com>
2014-12-08 15:23:50 -0500

    add image

--- index.php
+++ index.php
@@ -21,116 +21,6 @@
 	<h3>Please contact weisun@mit.edu for any question.</h3>
 	<hr>
 
-	<hr>
-	<form name="input_form" action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
-		<table class='table'>
-			<tr>
-				<th>
-					Upper counter
-				</th>
-				<td>
-					<label>Length:</label>
-					<div class="input-group">
-						<input class='form-control' type="number" name="l1" step=0.000001>
-						<div class="input-group-addon">cm</div>
-					</div>
-				</td>
-				<td>
-					<label>Width:</label>
-					<div class="input-group">
-						<input class='form-control' type="number" name="w1" step=0.000001>
-						<div class="input-group-addon">cm</div>
-					</div>  
-				</td>
-			</tr>
-			<tr>
-				<th>
-					Lower counter
-				</th>
-				<td>
-					<label>Length:</label>
-					<div class="input-group">
-						<input class='form-control' type="number" name="l2" step=0.000001>
-						<div class="input-group-addon">cm</div>
-					</div>
-				</td>
-				<td>
-					<label>Width:</label>
-					<div class="input-group">
-						<input class='form-control' type="number" name="w2" step=0.000001>
-						<div class="input-group-addon">cm</div>
-					</div>  
-				</td>
-			</tr>
-			<tr>
-				<th>
-					Offset Between Planes
-				</th>
-				<td>
-					<label>Length:</label>
-					<div class="input-group">
-						<input class='form-control' type="number" name="loff" step=0.000001>
-						<div class="input-group-addon">cm</div>
-					</div>
-				</td>
-				<td>
-					<label>Width:</label>
-					<div class="input-group">
-						<input class='form-control' type="number" name="woff" step=0.000001>
-						<div class="input-group-addon">cm</div>
-					</div>  
-				</td>
-			</tr>
-			<tr>
-				<th>
-					Distance Between Two Counters
-				</th>
-				<td>
-					<label>Width:</label>
-					<div class="input-group">
-					<input class="form-control" type="number" name="distance" step=0.000001>
-						<div class="input-group-addon">cm</div>
-					</div> 
-				</td>
-				</td>
-				<td>			
-			</tr>
-			<tr>
-				<th>
-					Normalized Rate
-				</th>
-				<td>
-					<label>Width:</label>
-					<div class="input-group">
-					<input class="form-control" type="number" name="norm_rate" value=1 step=0.000001>
-						<div class="input-group-addon">/cm2/min</div>
-					</div> 
-				</td>
-				</td>
-				<td>
-			</tr>
-			<tr>
-				<th>
-					Number of Generated MC Events:<br>
-				</th>
-				<td>
-					<label>Width:</label>
-					<div class="input-group">
-					<input class="form-control" type="number" name="n_event" value=40000>
-						<div class="input-group-addon">cm</div>
-					</div> 
-				</td>
-				</td>
-				<td>
-			</tr>
-		</table>
-		<br>
-		<div class="well">
-		<input type="submit" class='btn btn-primary btn-lg' name="submit" value="Calculate!"> 
-		</div>
-	</form>
-	<hr>
-
 <?php
 
 include_once("muon.php");
@@ -156,11 +46,11 @@ if($_POST)
 		echo "<h3>Parameters</h3>";
 		echo "<table class='table table-hover'>";
 		echo "<tr><td>Upper counter</td>";
-		echo "<td>Length: <b>".$l1."cm</b> &nbsp &nbsp &nbsp &nbsp Width: <b>".$w1."cm</b></td></tr>";
+		echo "<td>Length (al): <b>".$l1."cm</b> &nbsp &nbsp &nbsp &nbsp Width (aw): <b>".$w1."cm</b></td></tr>";
 		echo "<tr><td>Lower counter</td>";
-		echo "<td>Length: <b>".$l2."cm</b> &nbsp &nbsp &nbsp &nbsp Width: <b>".$w2."cm</b></td></tr>";
+		echo "<td>Length (bl): <b>".$l2."cm</b> &nbsp &nbsp &nbsp &nbsp Width (bw): <b>".$w2."cm</b></td></tr>";
 		echo "<tr><td>Offset Between Planes</td>";
-		echo "<td>Length: <b>".$loff."cm</b> &nbsp &nbsp &nbsp &nbsp Width: <b>".$woff."cm</b></td></tr>";
+		echo "<td>Length (fl): <b>".$loff."cm</b> &nbsp &nbsp &nbsp &nbsp Width (fw): <b>".$woff."cm</b></td></tr>";
 		echo "<tr><td>Distance between two counters &nbsp </td><td><b>".$distance."cm</b></td></tr>";
 		echo "<tr><td>Normalized rate</td><td><b>".$norm_rate."/cm2/min</b></td></tr>";
 		echo "</table>";
@@ -175,6 +65,118 @@ if($_POST)
 echo "<br><br><br><br>";
 
 ?>
+
+	<img src="scheme.png" style="width:750px;height=500px">
+
+	<hr>
+	<form name="input_form" action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
+		<table class='table'>
+			<tr>
+				<th>
+					Upper counter
+				</th>
+				<td>
+					<label>Length (al):</label>
+					<div class="input-group">
+						<input class='form-control' type="number" name="l1" step=0.000001>
+						<div class="input-group-addon">cm</div>
+					</div>
+				</td>
+				<td>
+					<label>Width (aw):</label>
+					<div class="input-group">
+						<input class='form-control' type="number" name="w1" step=0.000001>
+						<div class="input-group-addon">cm</div>
+					</div>  
+				</td>
+			</tr>
+			<tr>
+				<th>
+					Lower counter
+				</th>
+				<td>
+					<label>Length (bl):</label>
+					<div class="input-group">
+						<input class='form-control' type="number" name="l2" step=0.000001>
+						<div class="input-group-addon">cm</div>
+					</div>
+				</td>
+				<td>
+					<label>Width (bw):</label>
+					<div class="input-group">
+						<input class='form-control' type="number" name="w2" step=0.000001>
+						<div class="input-group-addon">cm</div>
+					</div>  
+				</td>
+			</tr>
+			<tr>
+				<th>
+					Offset Between Planes
+				</th>
+				<td>
+					<label>Length (fl):</label>
+					<div class="input-group">
+						<input class='form-control' type="number" name="loff" step=0.000001>
+						<div class="input-group-addon">cm</div>
+					</div>
+				</td>
+				<td>
+					<label>Width (fw):</label>
+					<div class="input-group">
+						<input class='form-control' type="number" name="woff" step=0.000001>
+						<div class="input-group-addon">cm</div>
+					</div>  
+				</td>
+			</tr>
+			<tr>
+				<th>
+					Distance Between Two Counters
+				</th>
+				<td>
+					<label>Height (h):</label>
+					<div class="input-group">
+					<input class="form-control" type="number" name="distance" step=0.000001>
+						<div class="input-group-addon">cm</div>
+					</div> 
+				</td>
+				</td>
+				<td>			
+			</tr>
+			<tr>
+				<th>
+					Normalized Rate
+				</th>
+				<td>
+					<label>Rate:</label>
+					<div class="input-group">
+					<input class="form-control" type="number" name="norm_rate" value=1 step=0.000001>
+						<div class="input-group-addon">/cm2/min</div>
+					</div> 
+				</td>
+				</td>
+				<td>
+			</tr>
+			<tr>
+				<th>
+					Number of Generated MC Events:<br>
+				</th>
+				<td>
+					<label>Number:</label>
+					<div class="input-group">
+					<input class="form-control" type="number" name="n_event" value=40000>
+						<div class="input-group-addon">#</div>
+					</div> 
+				</td>
+				</td>
+				<td>
+			</tr>
+		</table>
+		<br>
+		<div class="well">
+		<input type="submit" class='btn btn-primary btn-lg' name="submit" value="Calculate!"> 
+		</div>
+	</form>
+	<hr>
 
 </div>
 
